059cc6f0fc9aba7548c2b6311be5d990f9343861
Raymond Deng <1107450@gmail.com>
2024-05-29 23:49:20 -0700

    python poly comonoid

--- python/poly.py
+++ python/poly.py
@@ -1,6 +1,6 @@
-from typing import Callable, TypeVar
+from typing import Generic, TypeVar, Tuple, Callable, TypeVar
 
-T = TypeVar('T')
+T = TypeVar('T', covariant = True)
 
 class Poly(list):
     def __init__(self, coeffs: list[T]):
@@ -26,3 +26,48 @@ class Poly(list):
             for j, coeff2 in enumerate(other):
                 coeffs[i + j] += coeff1 * coeff2
         return Poly(coeffs)
+
+class Comonoid(Generic[T]):
+    def counit(self) -> None:
+        raise NotImplementedError
+
+    def comult(self) -> Tuple[T, T]:
+        raise NotImplementedError
+
+class PolyTerm(Generic[T]):
+    def __init__(self, coeff: T, exp: int):
+        self.coeff = coeff
+        self.exp = exp
+
+Poly = list[PolyTerm[T]]
+
+def poly_counit(p: Poly[T]) -> None:
+    if not p:
+        return
+    if p[0].exp == 0:
+        p[0].coeff.counit()
+    else:
+        raise ValueError("Polynomial must be constant to have a counit")
+
+def poly_comult(p: Poly[T]) -> Tuple[Poly[T], Poly[T]]:
+    xs = []
+    ys = []
+    for term in p:
+        if term.exp == 0:
+            c1, c2 = term.coeff.comult()
+            xs.append(PolyTerm(c1, 0))
+            ys.append(PolyTerm(c2, 0))
+        else:
+            xs.append(term)
+            ys.append(PolyTerm(term.coeff.counit(), term.exp))
+    return xs, ys
+
+class PolyComonoid(Comonoid[Poly[T]]):
+    def __init__(self, p: Poly[T]):
+        self.poly = p
+
+    def counit(self) -> None:
+        poly_counit(self.poly)
+
+    def comult(self) -> Tuple[Poly[T], Poly[T]]:
+        return poly_comult(self.poly)
