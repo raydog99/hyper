aaa08706efab070cd8e728081f8474ec7536d222
Raymond Deng <1107450@gmail.com>
2024-06-03 23:00:00 -0700

    haskell arena dual lens

--- haskell/lib/Arena.hs
+++ haskell/lib/Arena.hs
@@ -1,3 +1,5 @@
+import Data.Bifunctor (second)
+
 data Arena a = MkArena { pos :: a, dis :: a -> b }
 
 data Lens dom cod = MkLens { observe :: pos dom -> pos cod, interpret :: pos dom -> (pos cod -> dis cod) -> dis dom }
@@ -13,3 +15,11 @@ lens23 <.> lens12 = MkLens obs int
 
 type Display a = (pos a, dis a (pos a))
 type AsFunctor a b = (pos a, dis a (pos a) -> b)
+
+duoidal :: Arena a1 -> Arena a2 -> Arena b1 -> Arena b2 -> Lens ((a1 :<>: a2) :*: (b1 :<>: b2)) ((a1 :*: b1) :<>: (a2 :*: b2))
+duoidal a1 a2 b1 b2 =
+  let x = (a1 :<>: a2) :*: (b1 :<>: b2)
+      y = (a1 :*: b1) :<>: (a2 :*: b2)
+      o ((p1, p2), (q1, q2)) = ((p1, q1), \d -> (fst d $> p2, snd d $> q2))
+      i ((p1, p2), (q1, q2)) ((de1, de2) : Dis y (o ((p1, p2), (q1, q2)))) = ((de1 :** fst de2), (snd de1 :** snd de2))
+  in Lens o i
