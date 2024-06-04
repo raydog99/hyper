b9be9563b4c881c9c9ef78ce3832a1507163eaf3
Raymond Deng <1107450@gmail.com>
2024-06-03 23:29:00 -0700

    c++ arena dual lens

--- c++/include/arena.hpp
+++ c++/include/arena.hpp
@@ -36,3 +36,43 @@ using Display = std::pair<Pos, boost::function<Dis(Pos)>>;
 
 template <typename Pos, typename Dis, typename Res>
 using AsFunctor = std::pair<Pos, boost::function<Res(boost::function<Dis(Pos)>)>>;
+
+template <typename A1, typename A2, typename B1, typename B2>
+Lens<std::pair<boost::variant<A1, A2>, boost::variant<B1, B2>>,
+     boost::variant<std::pair<A1, B1>, std::pair<A2, B2>>>
+duoidal(const A1& a1, const A2& a2, const B1& b1, const B2& b2) {
+    using X = std::pair<boost::variant<A1, A2>, boost::variant<B1, B2>>;
+    using Y = boost::variant<std::pair<A1, B1>, std::pair<A2, B2>>;
+
+    auto x = std::make_pair(boost::variant<A1, A2>(a1, a2), boost::variant<B1, B2>(b1, b2));
+    auto y = boost::variant<std::pair<A1, B1>, std::pair<A2, B2>>(std::make_pair(a1, b1), std::make_pair(a2, b2));
+
+    auto o = [](const X& p) {
+        auto p1 = p.first;
+        auto p2 = p.second;
+        auto q1 = boost::get<A1>(p1);
+        auto q2 = boost::get<B1>(p2);
+
+        auto pp = std::make_pair(std::make_pair(q1, q2), [p1, p2](const Dis<std::pair<A1, B1>>& d) {
+            auto d1 = d.first;
+            auto d2 = d.second;
+            return std::make_pair(boost::apply_visitor(p1, d1), boost::apply_visitor(p2, d2));
+        });
+
+        return pp;
+    };
+
+    auto i = [](const X& p, const Dis<Y>& d) {
+        auto p1 = p.first;
+        auto p2 = p.second;
+        auto q1 = boost::get<A1>(p1);
+        auto q2 = boost::get<B1>(p2);
+
+        auto de1 = boost::get<std::pair<A1, B1>>(d);
+        auto de2 = boost::get<std::pair<A2, B2>>(d);
+
+        return std::make_pair(std::make_pair(de1.first, de2.first), std::make_pair(de1.second, de2.second));
+    };
+
+    return Lens<X, Y>(o, i);
+}
