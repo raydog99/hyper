e71148014fc87eb6070194e806e004141a5e0e23
Raymond Deng <1107450@gmail.com>
2024-06-03 23:07:00 -0700

    go dual lens

--- go/arena.go
+++ go/arena.go
@@ -38,3 +38,32 @@ type AsFunctor struct {
     pos interface{}
     dis func(interface{}) interface{}
 }
+
+func duoidal(a1, a2, b1, b2 Arena) Lens {
+    x := Pair(Combine(a1, a2), Combine(b1, b2))
+    y := Combine(Pair(a1, b1), Pair(a2, b2))
+
+    o := func(p Pos) Pos {
+        p1, p2 := p.First, p.Second
+        q1, q2 := p1.First, p1.Second
+
+        pp := func(d Dis) Pos {
+            return Pair(Combine(d.First, Apply(p2, d.First)), Combine(d.Second, Apply(q2, d.Second)))
+        }
+
+        return Pair(Pair(p1, q1), pp)
+    }
+
+    i := func(p Pos) func(d Dis) Dis {
+        p1, p2 := p.First, p.Second
+        q1, q2 := p1.First, p1.Second
+
+        return func(d Dis) Dis {
+            de1, de2 := d.First, d.Second
+
+            return Pair(Pair(de1.First, de2.First), Pair(de1.Second, de2.Second))
+        }
+    }
+
+    return Lens{o, i}
+}
