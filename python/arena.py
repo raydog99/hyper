fd8fc43f57bfea251f74015f6b26e1c4a24a42e1
Raymond Deng <1107450@gmail.com>
2024-06-03 23:54:47 -0700

    python arena dual lens

--- python/arena.py
+++ python/arena.py
@@ -24,3 +24,31 @@ def compose(lens23: Lens[A2, A3], lens12: Lens[A1, A2]) -> Lens[A1, A3]:
 
 Display = tuple[A, Callable[[A], B]]
 AsFunctor = tuple[A, Callable[[Callable[[A], B]], C]]
+
+A1 = TypeVar('A1', bound=Arena)
+A2 = TypeVar('A2', bound=Arena)
+B1 = TypeVar('B1', bound=Arena)
+B2 = TypeVar('B2', bound=Arena)
+
+Lens = Tuple[Callable[[Tuple[Tuple[A1, A2], Tuple[B1, B2]]], Tuple[Tuple[A1, B1], Callable[[Dis], Tuple[A2, B2]]]], Callable[[Tuple[Tuple[A1, A2], Tuple[B1, B2]], Dis], Dis]]
+
+def duoidal(a1: A1, a2: A2, b1: B1, b2: B2) -> Lens:
+    x = (combine(a1, a2), combine(b1, b2))
+    y = (pair(a1, b1), combine(pair(a2, b2)))
+
+    def o(p: Tuple[Tuple[A1, A2], Tuple[B1, B2]]) -> Tuple[Tuple[A1, B1], Callable[[Dis], Tuple[A2, B2]]]:
+        p1, p2 = p
+        q1, q2 = p1
+
+        def pp(d: Dis) -> Tuple[A2, B2]:
+            return (apply(p2, fst(d)), apply(q2, snd(d)))
+
+        return (p1, q1), pp
+
+    def i(p: Tuple[Tuple[A1, A2], Tuple[B1, B2]], d: Dis) -> Dis:
+        p1, p2 = p
+        q1, q2 = p1
+        de1, de2 = d
+        return (pair(fst(de1), fst(de2)), pair(snd(de1), snd(de2)))
+
+    return o, i
